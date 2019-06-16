aed35ddede46afb857d8dbac06766e157562373f
Anthony Molina <antonio-molina@att.net>
2019-06-16 05:34:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 class Dog 
   def name=(dog_name) 
-    fido = dog_name
+    fido.name = (dog_name)
   end 
   
   def name 
-    
+    fido.name = Fido
   end 
   
   def bark 
